9fe2abbf8ee551b807c6d72633c1c3d3b880ca7a
julio <juliocesar13b@gmail.com>
2026-04-27 20:10:36 -0300

    Initial commit

---
+++ src/main/java/com/devjulio/dscommerce/entities/User.java
@@ -0,0 +1,82 @@
+package com.devjulio.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity(name = "User")
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthate;
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String phone, LocalDate birthate, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.birthate = birthate;
+        this.password = password;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setI(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getBirthate() {
+        return birthate;
+    }
+
+    public void setBirthate(LocalDate birthate) {
+        this.birthate = birthate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
