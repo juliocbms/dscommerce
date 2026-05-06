75e7f9c3793f3c6f2a4bf5421b61c9290e2fd3ff
julio.cesar <juliocesar13b@mgial.com>
2026-05-06 08:07:46 -0300

    Customizando a resposta da validação

--- src/main/java/com/devjulio/dscommerce/controllers/handlers/ControllerExcpetionHandler.java
+++ src/main/java/com/devjulio/dscommerce/controllers/handlers/ControllerExcpetionHandler.java
@@ -1,11 +1,14 @@
 package com.devjulio.dscommerce.controllers.handlers;
 
 import com.devjulio.dscommerce.DTO.CustomError;
+import com.devjulio.dscommerce.DTO.ValidationError;
 import com.devjulio.dscommerce.services.exceptions.DatabaseException;
 import com.devjulio.dscommerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -26,6 +29,18 @@ public class ControllerExcpetionHandler {
     public ResponseEntity<CustomError> databaseException (DatabaseException e , HttpServletRequest request){
         HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError error = new CustomError(Instant.now(), status.value(),e.getMessage(), request.getRequestURI());
+        return  ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidException (MethodArgumentNotValidException e , HttpServletRequest request){
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError error = new ValidationError(Instant.now(), status.value(),"Dados inválidos", request.getRequestURI());
+
+        for (FieldError f : e.getBindingResult().getFieldErrors()){
+            error.addError(f.getField(),f.getDefaultMessage());
+        }
+
         return  ResponseEntity.status(status).body(error);
     }
 }
